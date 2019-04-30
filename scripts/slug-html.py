cc65f3a3b1c30cb448b10d09dda3757e13a25d5d
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-04-30 12:51:32 -0500

    identify post-content

--- scripts/slug-html.py
+++ scripts/slug-html.py
@@ -5,10 +5,35 @@
 
 # Standard library imports.
 from __future__ import division
+from html.parser import HTMLParser
 import sys
 import os
 import atexit
 import gc
+
+class JekyllSlugger(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        html = False
+        body = False
+        main = False
+        article = False
+        post = False
+
+    def handle_starttag(self, tag, attrs):
+        if not is_post_content_element(tag, attrs):
+            return
+        print("Encountered post content:", tag)
+
+def is_post_content_element(tag, attrs):
+    if tag != 'div':
+        return False
+
+    for k, v in attrs:
+        if k == 'class' and v == 'post-content':
+            return True
+
+    return False
 
 def on_exit():
     '''Actions to do on exit.'''
@@ -22,5 +47,7 @@ PN = os.path.splitext(sys.argv[0])[0]
 LOGF = ''.join([PN, '_log', '.txt'])
 
 atexit.register(on_exit)
+slugger = JekyllSlugger()
+slugger.feed(sys.stdin.read())
 
-print(sys.stdin.read(), end='')
+#print(sys.stdin.read(), end='')
