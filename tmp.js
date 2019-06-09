8c5e2a11b9c796975bcbc813512bfabf0eff91d9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-06-09 16:09:09 -0500

    add card

--- tmp.js
+++ tmp.js
@@ -1,17 +1,13 @@
 'use strict'; const log = console.log; (async ()=>{
 
-    static trickleDown(a, root, count) {
-        while(2*root + 1 < count) {
-            let child = 2*root + 1;
-            if(child + 1 < count && a[child] < a[child+1])
-                child++;
-            if(a[root] < a[child]) {
-                [a[root], a[child]] = [a[child], a[root]];
-                root = child;
-            }
-            else
-                return;
-        }
-    }
+const resolvedProm = Promise.resolve(33);
+
+const thenProm =
+  resolvedProm.then(val =>
+    (log(`asynchly called, val: ${val}`), val));
+
+log(thenProm);
+
+setTimeout(log, 0, thenProm);
 
 })();
