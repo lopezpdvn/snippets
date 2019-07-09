0d0f39e3bff620ae59afaecb8e6260acce4ef801
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-07-09 10:31:10 -0500

    add note

---
+++ notes/2019/gbjROdBoxZWBk7P3rcDt9FYh0aYL9dRg5tfX2Cn1.cs
@@ -0,0 +1,44 @@
+<Query Kind="Program">
+  <Output>DataGrids</Output>
+  <Namespace>System</Namespace>
+  <Namespace>System.Linq</Namespace>
+  <Namespace>System.Text</Namespace>
+</Query>
+
+//////////////////////////////////////////////////
+void Main() { A0(); }
+class Package {
+  public string Company { get; set; }
+  public double Weight { get; set; }
+  public long TrackNum { get; set; }
+}
+
+public static void A0() {
+  var packages = new List<Package> {
+    new Package { Company = "Coho Vineyard",
+      Weight = 25.2, TrackNum = 8945 },
+    new Package { Company = "Lucerne Pub.",
+      Weight = 18.7, TrackNum = 8911 },
+    new Package { Company = "Wingtip Toys",
+      Weight = 6.0, TrackNum = 2994 },
+    new Package { Company = "Contoso Pharma",
+      Weight = 9.3, TrackNum = 6700 },
+    new Package { Company = "Wide World",
+      Weight = 33.8, TrackNum = 4665 } };
+  $"|packages| = {packages.Count()}\n".Dump();
+
+  // q :: ILookup<char, anonymous type>
+  var q =
+    packages.ToLookup(
+      p => Convert.ToChar(
+             p.Company.Substring(0, 1)),
+      p => new { p.Company, p.TrackNum});
+
+  // x :: IGrouping<char, anonymous type>
+  foreach (var x in q) {
+    $"{x.Key}".Dump();
+    // y :: anonymous type
+    foreach (var y in x)
+      $"  {y.Company} {y.TrackNum}, ".Dump();
+  }
+}
