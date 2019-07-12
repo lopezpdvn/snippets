57f7f4fa83f97e4116dc919c3e57e8bec42e70d9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-07-11 22:41:06 -0500

    add note

---
+++ notes/2019/SgDPVt7BmVTUl0RQVcKsEsK8AOhtGrwea5C4VqAn.cs
@@ -0,0 +1,13 @@
+<Query Kind="Statements">
+  <Output>DataGrids</Output>
+  <Namespace>System</Namespace>
+  <Namespace>System.Linq</Namespace>
+  <Namespace>System.Text</Namespace>
+</Query>
+
+//////////////////////////////////////////////////
+int[] numbers = { 9, 34, 65, 92, 87, 435, 3, 54,
+                  83, 23, 87, 435, 67, 12, 19 };
+
+int first = numbers.First();
+first.Dump();
---
+++ notes/2019/ijz545Y5lPflcEuiCHGOoLsNlKTtXOkjKjEJ0SKW.md
@@ -0,0 +1,18 @@
+---
+layout: page
+title: home
+---
+
+{% highlight csharp %}
+int[] numbers = { 9, 34, 65, 92, 87, 435, 3, 54,
+                  83, 23, 87, 435, 67, 12, 19 };
+
+int first = numbers.First();
+first.Dump();
+{% endhighlight %}
+
+Output
+
+```
+9
+```
