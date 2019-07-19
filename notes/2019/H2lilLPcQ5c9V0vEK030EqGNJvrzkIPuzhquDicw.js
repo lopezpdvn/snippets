626b8595597d1a6c2d0eaadaf2b2cb97c03cb825
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-07-19 16:01:03 -0500

    add note

---
+++ notes/2019/H2lilLPcQ5c9V0vEK030EqGNJvrzkIPuzhquDicw.js
@@ -0,0 +1,15 @@
+'use strict'; const log = console.log; (async ()=>{
+
+class SortedArray {
+  static SelectionSort(a, n) {
+    for(let o = 0, min = 0; o < n - 1; min = ++o) {
+      for(let i = o + 1; i < n; i++)
+        if(a[min] > a[i])
+          min = i;
+      if(o !== min)
+        [a[o], a[min]] = [a[min], a[o]];
+    }
+  }
+}
+
+})();
