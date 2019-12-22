88018033a7db10059a4463532def086920d3e61e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-12-22 02:29:34 -0600

    update note

--- notes/2019/f4v2i2j1crz3r9i53zck3p7ec9cfbqhpm51tt4iy.js
+++ notes/2019/f4v2i2j1crz3r9i53zck3p7ec9cfbqhpm51tt4iy.js
@@ -1,8 +1,13 @@
-'use strict'; const log = console.log; (async ()=>{
-
+// Given a binary tree, determine if it's a valid
+// BST. Assume a BST is defined as follows:
+// - The left subtree of a node contains only
+//   nodes with keys less than the node's key.
+// - The right subtree of a node contains only
+//   nodes with keys greater than the node's key.
+// - Both the left and right subtrees must also
+//   be BSTs
 const isValidBST = root => {
-  return _isValidBST(root, null, null);
-};
+  return _isValidBST(root, null, null); };
 
 const _isValidBST = function f(root, min, max) {
   if(!root                     ) return true ;
@@ -14,12 +19,33 @@ const _isValidBST = function f(root, min, max) {
   return true;
 };
 
+// Complexity analysis
+// - Time: O(n)
+// - Space: O(n) worst
+
 class TreeNode {
-  constructor(x) {
-    this.val = x;
-    this.left = null;
-    this.right = null;
-  }
+  constructor(x) { this.val = x;
+    this.left = null; this.right = null; }
 }
 
-})();
+// Example 1:
+//
+//     2
+//    / \
+//   1   3
+//
+// Input: [2,1,3]
+// Output: true
+
+// Example 2:
+//
+//     5
+//    / \
+//   1   4
+//      / \
+//     3   6
+//
+// Input: [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but
+// its right child's value is 4.
