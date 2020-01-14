5d6c954cbe65340280e4ea29a12b66543aded9c9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-01-14 08:59:40 -0600

    add note

---
+++ notes/2020/6kq15bfs1c1zxhhv5aq2g7loof8u921y4gb7zzdl.js
@@ -0,0 +1,19 @@
+'use strict'; const log = console.log; (async ()=>{
+
+const x = new Map([
+  ['zero', 0],
+  ['two' , 2]
+]);
+
+let y;
+
+// 1 line
+const z = (y = x.get('zero'));
+
+log(y);
+log(z);
+
+// 0
+// 0
+
+})();
