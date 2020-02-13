48d29002b9f1fe45379477bcb4896d5698253f69
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-02-13 16:20:55 -0600

    add note

---
+++ notes/2020/4tryjcosgqvvz5bk6xdni6xt7eg1nnecqxo7vtqv.cs
@@ -0,0 +1,12 @@
+<Query Kind="Statements">
+  <Output>DataGrids</Output>
+</Query>
+
+//////////////////////////////////////////////////
+Type x;
+
+x = typeof(byte);
+
+x.FullName.Dump();
+
+// System.Byte 
