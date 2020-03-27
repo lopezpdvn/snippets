e767ec6d539b66895bb8927beba67be4cac9b5c7
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-03-27 17:12:56 -0600

    add snippet

---
+++ 2020/4jkk1tmbd8unja6g41ksump3r2xzfizadmeosp1i.cs
@@ -0,0 +1,43 @@
+<Query Kind="Program">
+  <Output>DataGrids</Output>
+</Query>
+
+//////////////////////////////////////////////////
+void Main() {
+  var A = new SortedSet<int>() {1, 2};
+  var B = new SortedSet<int>(A);
+
+  // A × B
+  var q = (from a in A
+           from b in B
+           select new[] {a, b})
+          .ToHashSet(new IntSeqEqCmp());
+  // B × A
+  var p = (from b in B
+           from a in A
+           select new[] { b, a })
+          .ToHashSet(new IntSeqEqCmp());
+  
+  q.SetEquals(p).Dump();
+  foreach (var x in new[] { q, p }) {
+    var pairs = string.Join(", ",
+      x.Select(y=> $"({y.First()}, {y.Last()})"));
+    $"{{{pairs}}}".Dump();
+  }
+}
+
+// EqualityComparer<t> : IEqualityComparer<T>
+class IntSeqEqCmp
+  : EqualityComparer<IEnumerable<int>>
+{
+  public override bool Equals(
+    IEnumerable<int> x, IEnumerable<int> y)
+      => x.SequenceEqual(y);
+
+  public override int GetHashCode(
+    IEnumerable<int> obj)
+      //=> obj.Sum(x => x.GetHashCode());
+      // Below prevents OverflowException
+      => obj.Aggregate(default(int),
+           (a, e) => unchecked(a + e.GetHashCode()));
+}
