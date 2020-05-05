6bc655f0c674bbf7a91d0a75910a2bab5160dbc1
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-05 03:58:19 -0500

    build In Degrees

--- 2020/w84l6rep6imuoj6z470nx6k1o9hfl6ugebamd699.js
+++ 2020/w84l6rep6imuoj6z470nx6k1o9hfl6ugebamd699.js
@@ -16,6 +16,17 @@ class Graph {
     uEdges.push(v);
     return this;
   }
+
+  topologicalSort() {
+    this.buildInDegrees();
+    log(this.inDegs);
+  }
+
+  buildInDegrees() {
+    this.inDegs = new Array(this.V).fill(0);
+    this.graph.forEach((v,u) =>
+      v.forEach(e => this.inDegs[e] += 1));
+  }
 }
 
 const g = new Graph(6).addEdge(5, 2).addEdge(5, 0)
@@ -23,5 +34,7 @@ const g = new Graph(6).addEdge(5, 2).addEdge(5, 0)
         .addEdge(3, 1);
 
 log(g);
+const y = g.topologicalSort();
+log(y);
 
 })();
