83f733760bf168dd89c645a2d1cf92db616ce114
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-05 04:34:37 -0500

    Implement topological sort

--- 2020/w84l6rep6imuoj6z470nx6k1o9hfl6ugebamd699.js
+++ 2020/w84l6rep6imuoj6z470nx6k1o9hfl6ugebamd699.js
@@ -24,13 +24,33 @@ class Graph {
       (a, inDeg, vertex) =>
         inDeg ? a : a.concat(vertex),
       []);
-    log(sourcesQueue);
+
+    let visitCnt = 0;
+    const topOrder = [];
+
+    while(sourcesQueue.length) {
+      const u = sourcesQueue.shift();
+      topOrder.push(u);
+
+      let neighbors;
+      if(undefined ===
+                (neighbors = this.graph.get(u)))
+        neighbors = [];
+      for(const v of neighbors)
+        if(!--this.inDegs[v]) {
+          sourcesQueue.push(v);
+        }
+
+      visitCnt++;
+    }
+
+    return visitCnt !== this.V ? [] : topOrder;
   }
 
   buildInDegrees() {
     this.inDegs = new Array(this.V).fill(0);
-    this.graph.forEach((v,u) =>
-      v.forEach(e => this.inDegs[e] += 1));
+    this.graph.forEach(v =>
+      v.forEach(e => this.inDegs[e]++));
   }
 }
 
@@ -38,8 +58,7 @@ const g = new Graph(6).addEdge(5, 2).addEdge(5, 0)
         .addEdge(4, 0).addEdge(4, 1).addEdge(2, 3)
         .addEdge(3, 1);
 
-log(g);
 const y = g.topologicalSort();
-//log(y);
+log(y);
 
 })();
