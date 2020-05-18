91df1a8dd94b9bd47beeb33f6b8223bf349b40e6
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-05-17 19:13:13 -0500

    add snippet

---
+++ 2020/7cn76ww0oc6eb25oh77orca14luxehne924bjls9.js
@@ -0,0 +1,25 @@
+'use strict'; const log = console.log; (async ()=>{
+
+// sum of digits of base-10 integer
+
+const f = x => {
+  x = Math.abs(x);
+  let sum = 0;
+
+  while(x > 0) {
+    sum += x % 10;
+    x = Math.trunc(x / 10);
+  }
+
+  return sum;
+};
+
+const X = [12, 0, 9202, -9202];
+X.forEach(x => log(f(x)));
+
+// 3
+// 0
+// 13
+// 13
+
+})();
