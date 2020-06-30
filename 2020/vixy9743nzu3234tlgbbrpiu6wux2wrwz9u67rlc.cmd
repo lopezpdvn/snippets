f27f4413f0653e0b0906223757e96d0f9f58e7cb
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-06-30 15:37:33 -0500

    tag snippet

--- 2020/vixy9743nzu3234tlgbbrpiu6wux2wrwz9u67rlc.cmd
+++ 2020/vixy9743nzu3234tlgbbrpiu6wux2wrwz9u67rlc.cmd
@@ -1,3 +1,6 @@
+:: Visual Studio pre/post build event, IIS application server pool recycle,
+:: restart
+
 copy /Y C:\_Projects\_Local\EnvName-14101\10.2.400.22\Deployment\Server\CSG\*dll C:\_Projects\_Local\EnvName-14101\10.2.400.4\Deployment\Server\CSG
 copy /Y C:\_Projects\_Local\EnvName-14101\10.2.400.22\Deployment\Server\CSG\*pdb C:\_Projects\_Local\EnvName-14101\10.2.400.4\Deployment\Server\CSG
 C:\Windows\System32\inetsrv\appcmd recycle apppool /apppool.name:EnvName-14101-10.2.400.22
