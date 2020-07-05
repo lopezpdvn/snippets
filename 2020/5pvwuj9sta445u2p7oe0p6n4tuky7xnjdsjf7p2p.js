701dc383153a58e3b10ab6043a4e5c776e8ab0ba
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-07-05 17:25:42 -0500

    progress

--- 2020/5pvwuj9sta445u2p7oe0p6n4tuky7xnjdsjf7p2p.js
+++ 2020/5pvwuj9sta445u2p7oe0p6n4tuky7xnjdsjf7p2p.js
@@ -11,24 +11,30 @@ decodings :: String -> [String]                 */
 const decodings = function* (s) {
   if(s === undefined || s === null)
     throw new Error();
-  yield* _g(s);
+  yield* _g(s.length, s)
 };
 
-const _g = function* g(i, s, memo) {
-  if(!s) {
-    yield [];
+const _g = function* g(k, s) {
+  if(!k) {
+    yield '';
     return;
   }
 
-  if(i === 0) {
-    const x = [dict[parseInt(s[i])]];
-    const digit2 = parseInt(s.slice(i, 2));
-    if(s.length > 1 && digit2 <= 26) {
-      log('hi');
-      x.push(dict[digit2]);
-    }
-    return x;
+  if(k === 1){
+    if(s[0] !== '0')
+      yield dict[parseInt(s[0])];
+    return;
   }
+
+  if(s[k-1] !== '0') {
+    const ck = dict[parseInt(s[k-1])];
+    for(const c of g(k-1, s)) {
+      yield c + ck;
+    }
+  }
+
+  return;
+
 
   if(!memo[i]) {
     memo[i] = g(i - 1, s, memo);
@@ -81,10 +87,9 @@ const dict = [
 ];
 
 const inputTest = [
-  ''
- // '',
-  //'1',
-  //'12',
+  '',
+  '1',
+  '12',
   //'226'
 ];
 
