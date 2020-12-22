d574b42fdda15a38acea798311fe8e144fc9d80d
Pedro Ivan Lopez <m@pedroivanlopez.com>
2020-12-22 16:36:13 -0600

    add snippet

---
+++ 2020/x2tz23cmefkteiv357ldvg0kd1rz6h0xqkj4iavq.py
@@ -0,0 +1,15 @@
+# Is node p an ancestor of node q?
+#
+# f :: BTNode -> BTNode -> Bool
+
+def f(p, q):
+    if is_equal(p, q):
+        return True
+    return p and (f(p.left, q) or f(p.right, q))
+
+def is_equal(p, q):
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    return p.value == q.value
