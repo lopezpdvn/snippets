b7da1c2514098269fa71a4353b628859a417ee51
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-01-12 20:41:05 -0600

    add snippet

---
+++ 2021/lobr0vo6qg1lcet0o7zxom22rq65eum83fdnbx3b.py
@@ -0,0 +1,36 @@
+def cartesian_prod(arrs):
+    if arrs is None:
+        raise Exception()
+
+    A = [[]]
+
+    for arr in arrs:
+        if arr is None:
+            raise Exception()
+
+        if not arr:
+            return []
+
+        _A = A
+        A = [[]] * (len(_A) * len(arr))
+
+        i = 0
+        for _a in _A:
+            for a in arr:
+                A[i] = [*_a, a]
+                i += 1
+
+    return A
+
+x = [[1,2], ['A'], ['x', 'y', 'z']]
+y = cartesian_prod(x)
+
+for tpl in y:
+    print(tpl)
+
+# [1, 'A', 'x']
+# [1, 'A', 'y']
+# [1, 'A', 'z']
+# [2, 'A', 'x']
+# [2, 'A', 'y']
+# [2, 'A', 'z']
