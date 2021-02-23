dc38534003231e9ec12f4573449c2f862a0c2acd
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-02-22 19:24:52 -0600

    Create tmp.hs

---
+++ 2021/tmp.hs
@@ -0,0 +1,14 @@
+data Vector a =
+  Vector a a a
+  deriving (Show)
+  
+vplus :: (Num t) =>
+         Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) =
+  Vector (i+l) (j+m) (k+n)
+
+x = Vector 3 5 8 `vplus` Vector 9 2 8
+
+main = putStrLn (show x)
+
+-- Vector 12 7 16
