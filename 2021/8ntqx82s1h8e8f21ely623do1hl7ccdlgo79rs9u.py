1c204320175b320102b69c55fec4b81c672d0fa0
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-05-24 21:17:13 -0500

    add tests

--- 2021/8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py
+++ 2021/8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py
@@ -16,12 +16,12 @@ def f(A, x):
         if A[M] == x: return M
 
         if A[L] <= A[R]:
-            if x < A[M]:
+            if A[L] <= x < A[M]:
                 R = M - 1
             else:
                 L = M + 1
         else:
-            if x > A[M]:
+            if A[L] >= x > A[M]:
                 R = M - 1
             else:
                 L = M + 1
@@ -32,6 +32,8 @@ assert f([], 5) == -1
 assert f([], 4) == -1
 assert f([4,6,10], 10) == 2
 assert f([1,2,3,4,5,6,7], 5) == 4
+assert f([1,1,1,1,5,6,7], 5) == 4
+assert f([1,2,3,4,4,4,4], 2) == 1
 assert f([10,6,4], 10) == 0
 assert f([10,6,4], 4) == 2
 assert f([2,2,2], 4) == -1
