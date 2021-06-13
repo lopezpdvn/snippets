5ad794d3270063d554b9d2396e31239720925e43
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-06-13 00:50:33 -0500

    Update ehk4jjpqehb3ula62zmdd1kqyzka804srx7o6myc.py

--- 2021/ehk4jjpqehb3ula62zmdd1kqyzka804srx7o6myc.py
+++ 2021/ehk4jjpqehb3ula62zmdd1kqyzka804srx7o6myc.py
@@ -1,4 +1,4 @@
-  def concatMap(f, x):
+def concatMap(f, x):
     from itertools import chain
     return chain(*map(f, x))
 
@@ -7,7 +7,5 @@ x = ('B,C', 'D,E,F', 'G,H')
 y = concatMap(lambda e: e.split(','), x)
 
 print((*y,))
-m itertools import chain
-    return chain(*map(f, x))
 
 # ('B', 'C', 'D', 'E', 'F', 'G', 'H')
