ea4a80351712c2a1941302c65b054ede46b73029
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-06-25 20:40:49 -0500

    use list comprehensions

--- 2021/te0d14iptucziwswuemg0h2sylvd39pbvfm20mg4.py
+++ 2021/te0d14iptucziwswuemg0h2sylvd39pbvfm20mg4.py
@@ -10,14 +10,15 @@
 # If needed to do a lot of times, by growing space to O(nm) where m is cardinality of charset, each lookup can be done in constant time.
 
 def f(s, c, i):
-    n = len(s)
-    nxt = [None] * n
-    last = [-1] * 26 # cardinality char set
-    for k in range(n-1, -1, -1):
-      last[ord(s[k]) - ord('a')] = k
-      nxt[k] = tuple(last)
+  n = len(s)
+  nxt = [None for i in range(n)]
+  last = [-1 for i in range(26)] #cardnlty charset
 
-    return nxt[i][ord(c) - ord('a')]
+  for k in range(n-1, -1, -1):
+    last[ord(s[k]) - ord('a')] = k
+    nxt[k] = tuple(last)
+
+  return nxt[i][ord(c) - ord('a')]
 
 assert f('abcdebdde', 'e', 7) == 8
 assert f('abcdebdde', 'c', 0) == 2
