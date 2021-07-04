7b59f9eb9e7ec1c7e858c98c19f01712ffcb3f7f
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-07-04 01:30:37 -0500

    Create icreazu0jyndxjb0dk4niuddpr7khyuhmuun3ogy.hs

---
+++ 2021/icreazu0jyndxjb0dk4niuddpr7khyuhmuun3ogy.hs
@@ -0,0 +1,17 @@
+type Stack = [Int]
+  
+pop :: Stack -> (Int,Stack)
+pop (x:xs) = (x,xs)
+
+push :: Int -> Stack -> ((),Stack)
+push a xs = ((),a:xs)
+
+stackManip :: Stack -> (Int, Stack)
+stackManip stack = let
+    ((),newStack1) = push 3 stack
+    (a ,newStack2) = pop newStack1
+    in pop newStack2
+
+main = print (stackManip [5,8,2,1])
+
+-- (5,[8,2,1])
