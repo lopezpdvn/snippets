8cc24c39d8d26d68d6c94f4f4946dad214e08ef9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-09-18 15:28:29 -0700

    Update 8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py

--- 2021/8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py
+++ 2021/8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py
@@ -1,42 +1,27 @@
 # agnostic binary search
 
-def f(A, target):
-    if not A:
-        return -1
-
+def f(A, x):
+    if not A: return -1
     n = len(A)
-    if n == 1:
-        return 0 if A[0] == target else -1
-
     L = 0
     R = n - 1
-
-    if A[L] == A[R]:
-        return 0 if A[0] == target else -1
-
     while L <= R:
-        M = L + (R - L) // 2
-        if A[M] == target:
-            return M
-
         if A[L] == A[R]:
-            return -1
-
+            return L if A[L] == x else -1
+        M = L + (R - L) // 2
+        if x == A[M]: return M
         if A[L] < A[R]:
-            if target < A[M]:
-                R = M - 1
-            else:
-                L = M + 1
+            if x < A[M]: R = M - 1
+            else:        L = M + 1
         else:
-            if target > A[M]:
-                R = M - 1
-            else:
-                L = M + 1
-
+            if x > A[M]: R = M - 1
+            else:        L = M + 1
     return -1
 
 assert f([], 5) == -1
 assert f([], 4) == -1
+assert f([3], 3) == 0
+assert f([4], 3) == -1
 assert f([4,6,10], 10) == 2
 assert f([1,2,3,4,5,6,7], 5) == 4
 assert f([1,1,1,1,5,6,7], 5) == 4
