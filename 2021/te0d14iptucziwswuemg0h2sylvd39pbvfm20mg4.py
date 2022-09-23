d310db0478144736ca15faad22e196d69935fb1a
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-09-22 18:39:40 -0700

    Update te0d14iptucziwswuemg0h2sylvd39pbvfm20mg4.py

--- 2021/te0d14iptucziwswuemg0h2sylvd39pbvfm20mg4.py
+++ 2021/te0d14iptucziwswuemg0h2sylvd39pbvfm20mg4.py
@@ -9,16 +9,16 @@
 
 # If needed to do a lot of times, by growing space to O(nm) where m is cardinality of charset, each lookup can be done in constant time.
 
-def f(s, c, i):
-  n = len(s)
-  nxt = [None for i in range(n)]
-  last = [-1 for i in range(26)] #cardnlty charset
+def f(s, char_search, istart):
+    n = len(s)
+    A = [None for i in range(n)]
+    chrdict = {}
+    for i in range(n - 1, -1, -1):
+        char = s[i]
+        chrdict[char] = i
+        A[i] = {**chrdict}
 
-  for k in range(n-1, -1, -1):
-    last[ord(s[k]) - ord('a')] = k
-    nxt[k] = tuple(last)
-
-  return nxt[i][ord(c) - ord('a')]
+    return A[istart][char_search]
 
 assert f('abcdebdde', 'e', 7) == 8
 assert f('abcdebdde', 'c', 0) == 2
