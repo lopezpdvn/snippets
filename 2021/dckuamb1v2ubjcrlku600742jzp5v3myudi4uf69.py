812d804bbad97bbee0fad609817c8e6ff197080d
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-09-28 08:30:51 -0700

    Update dckuamb1v2ubjcrlku600742jzp5v3myudi4uf69.py

--- 2021/dckuamb1v2ubjcrlku600742jzp5v3myudi4uf69.py
+++ 2021/dckuamb1v2ubjcrlku600742jzp5v3myudi4uf69.py
@@ -8,18 +8,16 @@
 # Ex3: 'ab'   -> ''   -> 'ab'
 # Ex4: ''     -> 'ab' -> 'ab'
 
-def f(s, t):
-    return ''.join(g(s, t))
+def g(a, b):
+    a_len = len(a)
+    b_len = len(b)
+    yield from (x + y for x, y in zip(a, b))
+    if a_len == b_len: return
+    seq_longest = a if a_len > b_len else b
+    n_remain = abs(a_len - b_len)
+    yield seq_longest[-n_remain:]
 
-def g(s, t):
-    yield from (''.join(x) for x in zip(s, t))
-    slen = len(s)
-    tlen = len(t)
-    if slen == tlen:
-        return
-    nremain = abs(slen - tlen)
-    str_remain = s if slen > tlen else t
-    yield str_remain[-nremain:]
+f = lambda a, b: ''.join(g(a, b))
 
 assert f('', '') == ''
 assert f('ab', 'cd') == 'acbd'
