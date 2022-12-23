f65f5ca9018e92010324dd606b1e34b6412646d9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-12-22 22:34:26 -0800

    Create state_monad_example.hs

---
+++ 2022/state_monad_example.hs
@@ -0,0 +1,48 @@
+-- The State monad is a monad that allows you to carry around a state in a
+-- computation. It's often used when you need to perform some computation that
+-- involves keeping track of some piece of state, and you want to do so in a way
+-- that is composable and easy to understand.
+
+-- Here's an example of how you might use the State monad in Haskell to
+-- implement a simple program that increments a counter:
+
+import Control.Monad.State
+
+-- Define a type alias for our state, which will be an Int
+type CounterState = State Int
+
+-- Define a function that increments the counter by 1
+incrementCounter :: CounterState ()
+incrementCounter = do
+  count <- get
+  put (count + 1)
+
+-- Define a function that gets the current value of the counter
+getCounter :: CounterState Int
+getCounter = get
+
+-- Define a function that runs a sequence of stateful actions
+runCounter :: CounterState a -> Int -> (a, Int)
+runCounter actions initialState = runState actions initialState
+
+-- Now we can use our stateful functions like this:
+main :: IO ()
+main = do
+  let (result, finalState) = runCounter (do
+                                         incrementCounter
+                                         incrementCounter
+                                         getCounter
+                                       ) 0
+  print result  -- prints 2
+  print finalState  -- prints 2
+
+-- In this example, the CounterState type alias represents a stateful
+-- computation that carries around an Int state. The incrementCounter function
+-- increments the counter by one, and the getCounter function gets the current
+-- value of the counter. The runCounter function runs a sequence of stateful
+-- actions, starting with an initial state, and returns the final state as well
+-- as the result of the computation.
+
+-- In the main function, we use runCounter to run a sequence of stateful actions
+-- that increments the counter twice and then gets the final value of the
+-- counter. The result of the computation is 2, and the final state is also 2.
