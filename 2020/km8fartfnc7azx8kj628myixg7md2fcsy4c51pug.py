0606e1eba5ae3eca62441bf0e3e2af1eb3cda238
Pedro Ivan Lopez <m@pedroivanlopez.com>
2023-02-04 18:43:24 -0800

    Update km8fartfnc7azx8kj628myixg7md2fcsy4c51pug.py

--- 2020/km8fartfnc7azx8kj628myixg7md2fcsy4c51pug.py
+++ 2020/km8fartfnc7azx8kj628myixg7md2fcsy4c51pug.py
@@ -1,16 +1,23 @@
-def partition(A, l, r):
-    i_pivot = l + (r - l) // 2;
-    pivot = A[i_pivot]
+def f(A, l=None, r=None):
+    l = l or 0
+    r = r or len(A) - 1
+    pivot = A[l + (r - l) // 2]
 
-    while l <= r:
-        while A[l] < pivot :
+    while l < r:
+        while A[l] < pivot:
             l += 1
         while pivot < A[r]:
             r -= 1
 
-        if l <= r:
-            A[l], A[r] = A[r], A[l]
-            l += 1
-            r -= 1
+        A[l], A[r] = A[r], A[l]
+        l += 1
+        r -= 1
 
-    return l
+    return A, l
+
+assert f([0])[0] == [0]
+assert f([0, 0])[0] == [0, 0]
+assert f([0, 0, -1])[0] == [-1, 0, 0]
+assert f([0, -1, -1])[0] == [-1, -1, 0]
+assert f([0, 1])[0] == [0, 1]
+assert f([1, 0])[0] == [0, 1]
