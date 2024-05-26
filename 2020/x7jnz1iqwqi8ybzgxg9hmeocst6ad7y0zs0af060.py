d096a5cc1c0d585ef03a5ac57ae8f787867deeab
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-05-25 18:23:56 -0700

    Update x7jnz1iqwqi8ybzgxg9hmeocst6ad7y0zs0af060.py

--- 2020/x7jnz1iqwqi8ybzgxg9hmeocst6ad7y0zs0af060.py
+++ 2020/x7jnz1iqwqi8ybzgxg9hmeocst6ad7y0zs0af060.py
@@ -1,27 +1,22 @@
-num2char = 'AABCDEFGHIJKLMNOPQRSTUVWXYZ'
+num2char = 'AABCDEFGHIJKLMNOPQRCTUVWXYZ'
 
-def g(s):
-    s = s or ''
-    n = len(s)
-    if not n:
+def g(C):
+    C = C or ''
+    if not C:
         yield ''
         return
-    if n == 1:
-        x = int(s[n - 1])
-        if x:
-            yield num2char[x]
+    if len(C) == 1:
+        if C != '0':
+            yield num2char[int(C)]
         return
-    x = int(s[n - 2:n])
-    if 10 <= x <= 26:
-        for prefix in g(s[:n-2]):
-            yield prefix + num2char[x]
-    x = int(s[n - 1])
-    if not x:
-        return
-    for prefix in g(s[:n-1]):
-        yield prefix + num2char[x]
+    if 10 <= int(C[-2:]) <= 26:
+        for prefix in g(C[:-2]):
+            yield prefix + num2char[int(C[-2:])]
+    if C[-1] != '0':
+        for prefix in g(C[:-1]):
+            yield prefix + num2char[int(C[-1])]
 
-f = lambda s: (*g(s),)
+f = lambda C: (*g(C),)
 
 assert f('') == (''   ,)
 assert f('1') == ('A'  ,)
@@ -31,4 +26,4 @@ assert f('22') == ('V', 'BB')
 assert f('226') == ('BZ', 'VF', 'BBF')
 assert f('111') == ('AK', 'KA', 'AAA')
 
-# A non-empty str containing letters A-Z is encoded to nums w/ A->1, B->2, ..., Z->26. Enumerate decodings.
+# A non-empty Ctr containing letterC A-Z iC encoded to numC w/ A->1, B->2, ..., Z->26. Enumerate decodingC.
