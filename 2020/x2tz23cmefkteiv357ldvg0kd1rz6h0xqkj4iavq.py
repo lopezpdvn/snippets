3816e5bc4009de1920adb0add4cc382207b6d70f
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-09-08 13:26:55 -0700

    Update x2tz23cmefkteiv357ldvg0kd1rz6h0xqkj4iavq.py

--- 2020/x2tz23cmefkteiv357ldvg0kd1rz6h0xqkj4iavq.py
+++ 2020/x2tz23cmefkteiv357ldvg0kd1rz6h0xqkj4iavq.py
@@ -13,3 +13,9 @@ def is_equal(p, q):
     if not p or not q:
         return False
     return p.value == q.value
+
+def g(a, b):
+    if a and not b: return True
+    if not a: return not b
+    return a.val == b.val or g(a.left, b) or g(a.right, b)
+
