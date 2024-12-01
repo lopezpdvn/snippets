f8a6a96683046e651d92e689cfe1b9342a618992
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-12-01 15:30:12 -0800

    Update mergesort.py

--- 2024/mergesort.py
+++ 2024/mergesort.py
@@ -1,15 +1,7 @@
-def _mergesort(A, l, r):
-    if l >= r: return A
-    m = l + (r - l) // 2
-    _mergesort(A, l, m)
-    _mergesort(A, m + 1, r)
-    merge(A, l, m, r)
-    return A
+def merge(A, l, m , r):
+    H, hl, hr, i = [*A[l:r+1]], 0, (r-l)//2+1, l
 
-def merge(A, l, m, r):
-    H, hl, hr, i = [*A], l, m + 1, l
-
-    while hl <= m and hr <= r:
+    while hl <= (r-l)//2 and hr <= r-l:
         if H[hl] <= H[hr]:
             A[i] = H[hl]
             hl += 1
@@ -18,13 +10,21 @@ def merge(A, l, m, r):
             hr += 1
         i += 1
 
-    while hl <= m:
+    while hl <= (r-l)//2:
         A[i] = H[hl]
         i += 1
         hl += 1
 
-def mergesort(A):
-    return _mergesort(A, 0, len(A) - 1)
+def mergesort(A, l=None, r=None):
+    l = 0 if l is None else l
+    r = len(A) - 1 if r is None else r
+    if l >= r: return A
+
+    m = l + (r-l) // 2
+    mergesort(A, l, m)
+    mergesort(A, m + 1, r)
+    merge(A, l, m, r)
+    return A
 
 tests = [
    [3, 4, 1, 0, 2],
