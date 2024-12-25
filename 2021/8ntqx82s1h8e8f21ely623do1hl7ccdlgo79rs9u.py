7159880c4bed63b4023f8ac5cf00bcdbc4010a1e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2024-12-25 14:29:26 -0800

    Update 8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py

--- 2021/8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py
+++ 2021/8ntqx82s1h8e8f21ely623do1hl7ccdlgo79rs9u.py
@@ -1,19 +1,31 @@
 # agnostic binary search
 
 def f(A, x):
-    L, R = 0, len(A) - 1
-    while L <= R:
-        if L == R or A[L] == A[R]:
-            return L if A[L] == x else -1
-        M = L + (R - L) // 2
-        if x == A[M]: return M
-        if A[L] < A[R]:
-            if x < A[M]: R = M - 1
-            else:        L = M + 1
-        else:
-            if x > A[M]: R = M - 1
-            else:        L = M + 1
-    return -1
+  l, r = 0, len(A) - 1
+
+  while l <= r:
+    if A[l] == A[r]:
+      return l if A[l] == x else -1
+    m = l + (r-l)//2
+    if A[m] == x:
+      if A[m-1] != A[m]:
+        return m
+      else:
+        r = m
+        continue
+    
+    if A[l] < A[r]:
+      if A[l] <= x < A[m]:
+        r = m - 1
+      else:
+        l = m + 1
+    else:
+      if A[l] >= x > A[m]:
+        r = m - 1
+      else:
+        l = m + 1
+
+  return -1
 
 assert f([], 5) == -1
 assert f([], 4) == -1
@@ -27,3 +39,4 @@ assert f([10,6,4], 10) == 0
 assert f([10,6,4], 4) == 2
 assert f([2,2,2], 4) == -1
 assert f([2,2,2], 2) == 0
+assert f([0,0,1,1,2,2,3,4,4,5,6], 2) == 4
